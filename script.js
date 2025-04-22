f41cc6ae59d129bc7040dbf79715f49ec26c409b
pietrosantos15 <pietro.santos.senai@gmail.com>
2025-04-22 12:54:13 -0300

    api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const ENDPOINT_BASE = 'http://127.0.0.1:5000/clientes';
+const ENDPOINT_BASE = 'https://back-seven-mauve.vercel.app/clientes';
 const ENDPOINT_LISTA_TODOS = `${ENDPOINT_BASE}/lista`;
 
 
